e8fd1b41a30098a6d7ed9fa7212670705e062af1
Akshay Joy <akshayjoyinfo@gmail.com>
2019-12-09 02:49:42 +0530

    added relavtive folder to deplouy

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(__dirname + '/dist/angular-socketio-whatsapp-client'));
 
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname,'/dist/angular-socketio-whatsapp-client/index.html'));
