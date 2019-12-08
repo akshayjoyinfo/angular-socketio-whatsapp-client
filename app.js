13c2e6685560cea52603a1ebda5679003f7d4bd2
Akshay Joy <akshayjoyinfo@gmail.com>
2019-12-09 02:31:33 +0530

    added engines for heroku

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist'));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname,'./dist/angular-socketio-whatsapp-client/index.html'));
+  res.sendFile(path.join(__dirname,'/dist/angular-socketio-whatsapp-client/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
